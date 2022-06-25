eab54efbe733c8d8d3c9babb07d2897de841e633
unknown <akashm006@outlook.com>
2022-06-25 22:01:47 +0530

    Changed routing by displaying links based on the type of the user

--- src/components/NavBar/SideBarData.js
+++ src/components/NavBar/SideBarData.js
@@ -5,51 +5,56 @@ import * as IoIcons from "react-icons/io";
 
 export const SidebarData = [
   {
-    title: "Book a Service Call",
+    title: "Book Service Call",
     path: "/user/book",
     icon: <AiIcons.AiFillHome />,
     cName: "nav-text",
+    allowedTypes: [1],
   },
   {
-    title: "Service Call Status",
+    title: "Service Calls Status",
     path: "/user/status",
     icon: <IoIcons.IoIosPaper />,
     cName: "nav-text",
+    allowedTypes: [1, 2, 3],
   },
-  {
-    title: "Service Call Allotment",
-    path: "/user/calls",
-    icon: <FaIcons.FaCartPlus />,
-    cName: "nav-text",
-  },
-  {
-    title: "Service Calls Completed",
-    path: "/user/calls",
-    icon: <IoIcons.IoMdPeople />,
-    cName: "nav-text",
-  },
-  {
-    title: "Service Calls",
-    path: "/user/calls",
-    icon: <FaIcons.FaEnvelopeOpenText />,
-    cName: "nav-text",
-  },
+  // {
+  //   title: "Service Call Allotment",
+  //   path: "/user/calls",
+  //   icon: <FaIcons.FaCartPlus />,
+  //   cName: "nav-text",
+  // },
+  // {
+  //   title: "Service Calls Completed",
+  //   path: "/user/calls",
+  //   icon: <IoIcons.IoMdPeople />,
+  //   cName: "nav-text",
+  // },
+  // {
+  //   title: "Service Calls",
+  //   path: "/user/calls",
+  //   icon: <FaIcons.FaEnvelopeOpenText />,
+  //   cName: "nav-text",
+  // },
   {
     title: "Alloted Service Calls",
     path: "/user/calls",
     icon: <IoIcons.IoMdHelpCircle />,
     cName: "nav-text",
+    allowedTypes: [2, 3],
   },
   {
     title: "Create User",
     path: "/user/createuser",
     icon: <IoIcons.IoMdHelpCircle />,
     cName: "nav-text",
+    allowedTypes: [3],
   },
   {
     title: "Logout",
     path: "/logout",
     icon: <IoIcons.IoIosLogOut />,
     cName: "nav-text",
+    allowedTypes: [1, 2, 3],
   },
 ];
